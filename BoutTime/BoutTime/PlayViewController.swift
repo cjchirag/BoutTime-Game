bb9e39cab8ba8903dfc323da0184922ea32b4aa5
Chirag <chirag@chiragjadhwani.com>
2018-07-08 12:59:23 +0530

    3

--- BoutTime/BoutTime/PlayViewController.swift
+++ BoutTime/BoutTime/PlayViewController.swift
@@ -16,9 +16,11 @@ class PlayViewController: UIViewController {
     @IBOutlet weak var secondEvent: UILabel!
     @IBOutlet weak var thirdEvent: UILabel!
     @IBOutlet weak var fourthEvent: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
     
     override func viewDidLoad() {
         self.becomeFirstResponder()
+        loadQuestion()
     }
     
     func loadQuestion() {
@@ -29,7 +31,48 @@ class PlayViewController: UIViewController {
         secondEvent.text = theQuestions[1]
         thirdEvent.text = theQuestions[2]
         fourthEvent.text = theQuestions[3]
+        runTimer()
     }
+    
+    @IBAction func oneToTwo(_ sender: Any) {
+        guard let temp = firstEvent.text else { return }
+        firstEvent.text = secondEvent.text
+        secondEvent.text = temp
+    }
+    
+    @IBAction func twoToOne(_ sender: Any) {
+        guard let temp = secondEvent.text else { return }
+        secondEvent.text = firstEvent.text
+        firstEvent.text = temp
+    }
+    
+    @IBAction func twoToThree(_ sender: Any) {
+        guard let temp = secondEvent.text else { return }
+        secondEvent.text = thirdEvent.text
+        thirdEvent.text = temp
+    }
+    
+    @IBAction func threeToTwo(_ sender: Any) {
+        guard let temp = thirdEvent.text else { return }
+        thirdEvent.text = secondEvent.text
+        secondEvent.text = temp
+    }
+    
+    @IBAction func threeToFour(_ sender: Any) {
+        guard let temp = fourthEvent.text else { return }
+        fourthEvent.text = thirdEvent.text
+        thirdEvent.text = temp
+    }
+    
+    @IBAction func fourToThree(_ sender: Any) {
+        guard let temp = thirdEvent.text else { return }
+        thirdEvent.text = fourthEvent.text
+        fourthEvent.text = temp
+    }
+    
+    
+    
+    
     func loadScore() {
         firstEvent.isHidden = true
         secondEvent.isHidden = true
@@ -59,7 +102,37 @@ class PlayViewController: UIViewController {
         }
     }
     
+    var seconds = 60
+    var timer = Timer()
+    var isTimerRunning = false
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(PlayViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    @objc func updateTimer() {
+        gameManager.questionsAnswered += 1
+        while seconds >= 0 {
+            timerLabel.text = "\(seconds)" //This will update the label.
+            if seconds == 0 {
+                guard let currentQs = currentEvent else {return}
+                do {
+                    let checkAnswer = try gameManager.checkFor(theEvent: currentQs, first: firstEvent.text!, second: secondEvent.text!, third: thirdEvent.text!, fourth: fourthEvent.text!)
+                    if checkAnswer {
+                        // FIXME: Labels
+                        loadNextRound(delay: 2)
+                    } else {
+                        loadNextRound(delay: 2)
+                    }
+                }
+                catch {}
+             seconds -= 1     //This will decrement(count down)the seconds.
+
+            }
+        }
+    }
+    
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        gameManager.questionsAnswered += 1
         print("Device was shaken!")
         guard let currentQs = currentEvent else {return}
         do {
@@ -72,6 +145,10 @@ class PlayViewController: UIViewController {
         } catch {
             
         }
+        timer.invalidate()
+        seconds = 60    //Here we manually enter the restarting point for the seconds, but it would be wiser to make this a variable or constant.
+        timerLabel.text = "\(seconds)"
+        loadNextRound(delay: 2)
     }
     
     
